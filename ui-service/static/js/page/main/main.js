5cc0b81e5067a81e511739cf2c33f516a36c8f2d
IcedSoul <1443700905@qq.com>
2019-07-08 14:42:46 +0800

    change js structure

--- ui-service/static/js/page/main/main.js
+++ ui-service/static/js/page/main/main.js
@@ -256,44 +256,6 @@ function onLineStatusNotice(type) {
     sendMessage(content, usersId, type);
 }
 
-function showOfflineMessage() {
-    let allMessages = getOfflineMessage()
-    for (let i = 0; i < allMessages.length; i++) {
-        let usersId = [];
-        usersId[0] = allMessages[i].toId;
-        let jsonMessage = JSON.stringify({
-            from: allMessages[i].fromId,
-            to: usersId,
-            content: allMessages[i].content,
-            type: allMessages[i].type,
-            time: allMessages[i].time
-        });
-        showReceiveMessage(allMessages[i].content, allMessages[i].fromId, usersId, allMessages[i].type, allMessages[i].time, jsonMessage);
-    }
-}
-
-function getOfflineMessage() {
-    let allMessages = null;
-    $.ajax({
-        async: false, //设置同步
-        type: 'GET',
-        url: address + 'v1/offlineMessages/toId/' + currentUser.userId,
-        dataType: 'json',
-        success: function (result) {
-            if (result.status === 1) {
-                allMessages = result.content;
-            }
-            else {
-                layer.alert('查询错误');
-            }
-        },
-        error: function (result) {
-            layer.alert('网络异常');
-        }
-    });
-    allMessages = eval("(" + allMessages + ")");
-    return allMessages;
-}
 
 function changeToOnlineStatus(content, from, to, type, time, message) {
     let onLineStatus = document.getElementById(from + 'onlineStatus');
